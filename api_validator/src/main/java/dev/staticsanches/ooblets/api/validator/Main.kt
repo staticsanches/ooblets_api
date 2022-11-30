023220dc64bf7c6eb78f1a2793afbff3a2297228
Felipe Sanches <staticsanches@gmail.com>
2022-11-29 21:08:59 -0300

    Adding timestamp_version file

--- api_validator/src/main/java/dev/staticsanches/ooblets/api/validator/Main.kt
+++ api_validator/src/main/java/dev/staticsanches/ooblets/api/validator/Main.kt
@@ -1,8 +1,14 @@
 package dev.staticsanches.ooblets.api.validator
 
+import kotlin.io.path.nameWithoutExtension
+
 fun main() {
     ApiPath.root.children
+        .filter { it.isDirectory }
         .forEach(AllFilesValidator::validate)
+    ApiPath.root.children
+        .filter { it.isFile }
+        .forEach { require(it.path.nameWithoutExtension == "timestamp_version") }
     ApiPath.root.children
         .forEach(DataFilesValidator::validate)
     ImageFilesValidator.validate(ApiPath.root["items"])
